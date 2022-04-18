40dac11cb67ba0cde757c7c5f885d0b53dab9632
moran <moran.abitbul1@gmail.com>
2022-04-18 12:42:23 +0300

    add get taxt

--- server/nlp/nltkTest.py
+++ server/nlp/nltkTest.py
@@ -5,10 +5,13 @@ sentence = "the occupation of taking and printing photographs or making movies"
 
 # Tokenize and tag some text:
 tokens = nltk.word_tokenize(sentence)
+print('tokens: ')
 print(tokens)
 tagged = nltk.pos_tag(tokens)
+print('tagged[0:6]: ')
 print(tagged[0:6])
 
 # Identify named entities:
 entities = nltk.chunk.ne_chunk(tagged)
+print('entities: ')
 print(entities)
--- server/visuals/updateVisualSlide.js
+++ server/visuals/updateVisualSlide.js
@@ -7,12 +7,16 @@ let pptx = new PPTX.Composer();
 
 
 const printXml = async () => {
-    const pre = await pptx.load('format.pptx');
-    var xmlDataSlide = pre.getSlide(5).getSlideXmlAsString();
-    console.log(xmlDataSlide);
+    const pre = await pptx.load('semi.pptx');
+    // var xmlDataSlide = pre.getSlide(4).getSlideXmlAsString();
+    // console.log(xmlDataSlide);
+    arr = pre.getSlide(1).getArrText();
+    console.log(arr[0]);
+   // console.log(arr[1]);
+
 }
 
-//printXml();
+printXml();
 
 const start = async () => {
     const pre = await pptx.load('test.pptx');
@@ -21,43 +25,42 @@ const start = async () => {
         console.log('start with slide - ' + i);
         var newColorText = new Array();
 
-        var xmlDataSlide = pre.getSlide(i).getSlideXmlAsString();
+        //var xmlDataSlide = pre.getSlide(i).getSlideXmlAsString();
         // xml to json
-        var dataToJson = parser.toJson(xmlDataSlide);
-        var json = JSON.parse(dataToJson);
+        // var dataToJson = parser.toJson(xmlDataSlide);
+        // var json = JSON.parse(dataToJson);
 
-       // console.log(json)
+        //backgroundColor = data.extractBackgroundColor(json);
+        //console.log("background color: " + backgroundColor);
 
-        backgroundColor = data.extractBackgroundColor(json);
-        console.log("background color: " + backgroundColor);
-
-        // backgroundColor = pre.getSlide(i).getBackgroundColor();
+        backgroundColor = pre.getSlide(i).getBackgroundColor();
         // console.log("background color: " + backgroundColor);
 
         colorText = pre.getSlide(i).getArrColors();
-        console.log("arr first: " );
-        console.log(colorText);
+        //console.log(colorText);
 
         // colorText = data.getArrayOfColorPara(json)
         // console.log("color text: " + colorText);
 
+        shapeTextArray = pre.getSlide(i).getArrText();
+        //console.log(shapeTextArray);
+
         // textArray = data.getArrayOfTextPara(json);
         // console.log("text array: " + textArray);
 
+        //update the background color with the first text
         updateBackground = await fix.checkContrast(backgroundColor, colorText[0][0]).then(array => {
             return array[0]; //colorbackground and text first
         });
-
-
-        console.log("updateBackground: " + updateBackground);
+        //console.log("updateBackground: " + updateBackground);
 
         //if there more then 1 color of text
         for (let j = 0; j < colorText.length; j++) { //num of shape
             newColorText[j] = new Array();
             for (let k = 0; k < colorText[j].length; k++) { //num of row text in one shape
-                console.log("before: colorText[" + j + k + "] " + colorText[j][k]);
+                // console.log("before: colorText[" + j + k + "] " + colorText[j][k]);
                 await fix.textContrast(updateBackground, colorText[j][k]).then(colorText => {
-                    console.log("atfer contrast colorText: " + colorText);
+                    //console.log("after contrast colorText: " + colorText);
                     newColorText[j].push(colorText)
                 });
             }
@@ -67,13 +70,13 @@ const start = async () => {
         // change slide
         var slide_i = pre.getSlide(i);
         console.log("change color in slide: " + i);
-        console.log(newColorText);
+        //console.log(newColorText);
         slide_i.backgroundColor(updateBackground);
         for (let i = 0; i < newColorText.length; i++) { //index for shape
-            console.log("i: " + i);
+            // console.log("i: " + i);
             for (let j = 0; j < newColorText[i].length; j++) { //index for row in 
-                console.log("j: " + j);
-                console.log(" color to change: " + newColorText[i][j]);
+                // console.log("j: " + j);
+                // console.log(" color to change: " + newColorText[i][j]);
                 slide_i.textColorToChange(newColorText[i][j], i, j);
             }
         }
@@ -86,5 +89,5 @@ const start = async () => {
 
 }
 
-start();
+//start();
 
