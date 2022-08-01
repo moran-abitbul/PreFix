573c2aaf6b814b2a03b95172533a43910937c116
moran-abitbul <82618998+moran-abitbul@users.noreply.github.com>
2022-08-01 19:31:52 +0300

    update video url

--- server/extensions/getVideoUrl.js
+++ server/extensions/getVideoUrl.js
@@ -1,21 +1,30 @@
 const videoUrlFile = require('./getVideoUrlApi')
 const spawn = require('child_process').spawn
+const path = require('path');
+
 
 // Get text and title --> run BOW in python file --> get the frequency words --> return video url according them
 const getVideoFromText = async (text) => {
     return await new Promise((resolve) => {
-        const bagOfWordsFile = spawn('python', ['../nlp/bagOfWord.py', text])
+        const BowFilePath = path.join(__dirname, `../nlp/bagOfWord.py`)
+        const bagOfWordsFile = spawn('python', [BowFilePath, text])
 
         //get the frequency words of the text from python file
         bagOfWordsFile.stdout.on('data', async (freqWord) => {
 
             //getVideoUrl
             //freqWord = search query 
-            await videoUrlFile.getVideoFromText(freqWord).then((resVideoUrl) => {
-                resolve(resVideoUrl)
+            await videoUrlFile.getVideoFromText(freqWord).then((objVideo) => {
+                resolve(objVideo.link)
             });
         });
     })
 }
 
+// text = 'Machine Learning: Allow the computer to learn from examples, Runs on a variety of computational tasks where classic programming is not possible. Supervised Learning. Unsupervised Learning. Reinforcement Learning. ',
+//     'Data Set: Data For Learning, Examples - with or without “results”. Occam’s Razor: prefer a simple model if possible. ',
+//     'Machine Learning Text Mining Learning Algorithm Text mining for market prediction Conclusions ',
+//     'Technology development. Documents go digital: Need to search for information digitally. Strong field, a lot of money, very popular. ',
+//     'Generalize to new examples: The ability of the model to properly adapt to previously unseen data. Dimensionality Reduction: Data from a high-dimensional space into a low-dimensional. NLP – Natural Language Processing. '
+// getVideoFromText(text).then();
 module.exports = { getVideoFromText }
--- server/extensions/getVideoUrlApi.js
+++ server/extensions/getVideoUrlApi.js
@@ -7,38 +7,67 @@ const getVideoFromText = async (search_query) => {
     //sp = CAMSAhgB = < 4 min and num views
     console.log("q: " + search_query);
 
-    axios
-        .get(`https://serpapi.com/search.json?engine=youtube&search_query=${search_query}&tbm=vid&sp=CAASBBABGAE%253D&api_key=${api_key}`)
-        .then(res => {
-            console.log(res.data.video_results)
+    return new Promise((resolve) => {
+        try {
+            axios
+                .get(`https://serpapi.com/search.json?engine=youtube&search_query=${search_query}&tbm=vid&sp=CAASBBABGAE%253D&api_key=${api_key}`)
+                .then(res => {
+                    // console.log(res.data.video_results)
 
-            var videoArr = res.data.video_results
-            //console.log("before sort:" + videoLength)
+                    var videoArr = res.data.video_results
+                    //console.log("before sort:" + videoLength)
 
 
-            videoLength = videoArr.map(element => element.length);
-            videoViews = videoArr.map(element => element.view);
+                    const getNumber = time => +time.replace(/:/g, '')
 
-            const getNumber = time => +time.replace(/:/g, '')
+                    //sort by length
+                    videoArr.sort((a, b) => {
 
-            //sort by length
-            videoArr.sort((a, b) => {
-                return getNumber(a.length) - getNumber(b.length)
-            })
+                        let a_floatNuumber = parseFloat(a.length);
+                        let b_floatNuumber = parseFloat(b.length);
 
-            //sort by views num
-            videoArr.sort((a, b) => {
-                return getNumber(a.views) - getNumber(b.views)
-            })
+                        // console.log("time: " + (a_floatNuumber - b_floatNuumber));
 
-            videoLength = videoArr.map(element => element.length);
-            //console.log("after sort:" + videoLength)
+                        // return getNumber(a.length) - getNumber(b.length)
+                        return (a_floatNuumber - b_floatNuumber);
+                    })
 
-            //resolve(res.data.video_results)
-        }).catch(err => {
+                    videoLength = videoArr.map(element => element.length);
+                    console.log("after sort Length:" + videoLength)
+
+                    var arrTopVideoLength = []
+
+                    for (var i = 0; i < videoLength.length; i++) {
+                        let time = videoLength[i].replace(':', '.')
+                        if (time >= 0.15 && time < 1) {
+                            arrTopVideoLength.push(videoArr[i])
+                        }
+
+                    }
+                    console.log("arrTopVideoLength ---> ");
+                    console.log(arrTopVideoLength);
+
+                    //sort by views num
+                    arrTopVideoLength.sort((a, b) => {
+                        let a_floatNuumber = parseFloat(a.views);
+                        let b_floatNuumber = parseFloat(b.views);
+                        //  return getNumber(a.views) - getNumber(b.views)
+                        return (a_floatNuumber - b_floatNuumber);
+                    })
+                    videoViews = arrTopVideoLength.map(element => element.views);
+                    console.log("after sort Views:" + videoViews)
+                    console.log(arrTopVideoLength[videoViews.length - 1]);
+
+                    resolve(arrTopVideoLength[videoViews.length - 1])
+                })
+        } catch (err) {
             console.error(err)
-        })
+        }
+    })
 }
+
+
+
 
 //getVideoFromText('semaphore operating system')
 module.exports = { getVideoFromText };
